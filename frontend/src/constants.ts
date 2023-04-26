267b3411a48f850d68f7e4889fe2cba2b5cf83b2
Sicoe-Sergiu <sergiusicoemarian@gmail.com>
2023-04-26 11:07:57 +0300

    Project commit - frontend V.1

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,4 @@
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
+// const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
+const DEV_BACKEND_API_URL = "http://ec2-16-16-79-182.eu-north-1.compute.amazonaws.com";
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;
